802129e197df957e4768f79e5cf57251f7f39c64
Txss-17 <gliyetat@gmail.com>
2026-09-09 21:38:53 +0200

    Update BibTalent.tsx

--- src/pages/BibTalent.tsx
+++ src/pages/BibTalent.tsx
@@ -32,10 +32,11 @@ export default function BibTalent() {
             </div>
           </div>
         </section>
-        <section>
-          <button onClick={() => window.open("https://docs.google.com/forms/d/e/1FAIpQLSemotUUORqFwUdM60S1BAW-79YzSm-5TiocRvm-wuTUiCevvg/viewform", "_blank")}>
-            Join BIB Talent
-          </button>
+        
+        <section className="relavtive overflow-hiden bg-bib-white">
+            <button onClick={() => window.open("https://docs.google.com/forms/d/e/1FAIpQLSemotUUORqFwUdM60S1BAW-79YzSm-5TiocRvm-wuTUiCevvg/viewform", "_blank")}>
+              Join BIB Talent
+            </button>
         </section>
       </main>
     </div>
