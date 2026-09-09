ef703da86d2b72f38590b4a80812d38f7e4c80ae
Txss-17 <gliyetat@gmail.com>
2026-09-09 18:22:48 +0200

    Update BibTalent.tsx

--- src/pages/BibTalent.tsx
+++ src/pages/BibTalent.tsx
@@ -5,12 +5,4 @@ import { Button } from "@/components/ui/button";
 import { useSEO } from "@/hooks/useSEO";
 
 
-export default function BibTalent() {
-  useSEO({
-    title: "BIB Talent — Brand-In-A-Box",
-    description:
-      "Découvrez BIB Talent, le programme Early Team de Brand-in-a-box, et explorez les opportunités de contribuer au développement du projet en Tech, Produit, R&D et Communication",
-  });
-
- return ();
-}
+export default ();
