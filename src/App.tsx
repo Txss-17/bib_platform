6b2629eddd5544e93dbb612604cacd7c41494df7
Txss-17 <gliyetat@gmail.com>
2026-09-18 00:09:41 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ import StoreCatalog from "@/pages/StoreCatalog";
 import StoreCart from "@/pages/StoreCart";
 import Recycler from "@/pages/customer/Recycler";
 import Unsubscribe from "@/pages/Unsubscribe";
-import StoreProduct from "@/components/store/StoreProduct";
+import StoreProduct from "@pages/StoreProduct";
 
 /* =====================================================
    SUPPLIERS / OPERATIONS
